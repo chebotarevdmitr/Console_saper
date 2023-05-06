321782a450f2e8c1acc45abbb07047ccf4899ba2
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-06 22:40:40 +0300

    field output

--- Console_saper.cpp
+++ Console_saper.cpp
@@ -3,7 +3,29 @@
 using namespace std;
 
 int field[100][100];
+// -1 mine (mina)
+// 0-8 cell (kletka)
+
+// pole igrorka  ->(field_p)<-
+int field_p[100][100];
+// -2  -clous
+// -1  -flag
+// 0-8 -opened    
+/* funkciya kotoraya chistit i vivodit pole na ekran 
+   -> print_field <- (){system("cls");for()...}
+*/
 const int N = 9, M = 9, K = 10;
+void print_field() {
+    system("cls");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (field_p[i][j] == -2)cout << "_ " ;
+            else if (field_p[i][j] == -1)cout << "f " ;
+            else cout << field_p[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
@@ -11,6 +33,7 @@ int main()
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             field[i][j] = 0;
+            field_p[i][j] = -2;
         }
     }
     for (int i = 0; i < K; ++i) {
@@ -38,12 +61,8 @@ int main()
        
         
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (field[i][j] == -1)cout << "*" << " ";
-            else cout<< field[i][j]<<" ";
-        }
-        cout << endl;
-    }
-  
+   
+    print_field();
+
+    system("pause>0");
 }
