51d99f843a8cef9fd9f80b2b3652af2ada06388e
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-07 22:40:09 +0300

    function is_win

--- Console_saper.cpp
+++ Console_saper.cpp
@@ -14,7 +14,7 @@ int field_p[100][100];
 /* funkciya kotoraya chistit i vivodit pole na ekran 
    -> print_field <- (){system("cls");for()...}
 */
-const int N = 11, M = 11, K = 10;
+const int N = 5, M = 5, K = 5;
 void print_field() {
     system("cls");
     cout << "  ";
@@ -27,16 +27,30 @@ void print_field() {
         cout << i + 1 << " ";
         if (i + 1 < 10)cout << ' ';
         for (int j = 0; j < M; j++) {
-            if (field_p[i][j] == -2)cout << "_  " ;
-            else if (field_p[i][j] == -1)cout << "f  " ;
+            if (field_p[i][j] == -2)cout << "_  ";
+            else if (field_p[i][j] == -1)cout << "f  ";
+            else if (field_p[i][j] == -3)cout << "*  ";
             else cout << field_p[i][j] << "  ";
         }
         cout << endl;
     }
+    cout << endl << endl << endl << endl;
 }
 
-//funkciya ->dfs<- "obhod v glubinu"(DIIP FIRST
+//funkciya ->dfs<- "obhod v glubinu"(DIIP FIRST SEARCH)
+//pri otkritii pustoy kletki otkrivautsya sosednie 
+//bezopastnie kletki
 
+void dfs(int x, int y) {
+    if (x < 0 || x >= N || y < 0 || y >= M || field_p[x][y]> -2) return;
+    field_p[x][y] = field[x][y];
+    if (field[x][y] > 0)return;
+    for (int i2 = x - 1; i2 <= x + 1; ++i2) {
+        for (int j2 = y - 1; j2 <= y + 1; ++j2) {
+            if (i2 != x || j2 != y) dfs(i2, j2);
+        }
+    }
+}
 
 //funkciya otkritiya kletki ->open_cell<-
 //true ne vzorvalis
@@ -46,7 +60,18 @@ bool open_cell(int x, int y) {
         field_p[x][y] = field[x][y];
         return true;
     }
-    dfs(x,y)
+    dfs(x, y);
+}
+
+//funkciya -> is_win <- proverka viigrali ili net
+bool is_win() {
+    int opened = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (field_p[i][j] >= 0) opened++;
+        }
+    }
+    return(N * M - K == opened);
 }
 
 int main()
@@ -101,14 +126,42 @@ int main()
                 // Vvod koordinat
                 int x, y;
                 cin >> x >> y;
+                --x; --y;
                 // otkroem kletki
-                if (x < 1 || x > N || y < 1 || y > M || field_p[x][y] >= 0) continue;
+                if (x < 0 || x >= N || y < 0 || y >= M || field_p[x][y] >= 0) continue;
+                if(! open_cell(x, y)) {
+                    for (int i = 0; i < N; i++) {
+                        for (int j = 0; j < M; j++) {
+                            if (field_p[i][j] == -1)field_p[i][j] = -2;
+                            if (field[i][j] == -1) field_p[x][y] = -3;
+                        }
+                    }
+                    print_field();
+                    cout << " Vi proigrali,\nchtob nacat novuyu igru vvedite lyubuyu stroku";
+                    string s;
+                    cin >> s;
+                    break;
+                }
+                if (is_win()) {
+                    for (int i = 0; i < N; i++) {
+                        for (int j = 0; j < M; j++) {
+                            if (field_p[i][j] == -1)field_p[i][j] = -2;
+                            if (field[i][j] == -1) field_p[x][y] = -3;
+                        }
+                    }
+                    print_field();
+                    cout << " Vi viigrali!!!!!,\nchtob nacat novuyu igru vvedite lyubuyu stroku";
+                    string s;
+                    cin >> s;
+                    break;
+                }
             }
             else if (comand == "\\f") {
                 // Vvod koordinat
                 int x, y;
                 cin >> x >> y;
-                if (x < 1 || x > N || y < 1 || y > M || field_p[x][y] >= 0) continue;
+                --x; --y;
+                if (x < 0 || x >= N || y < 0 || y >= M || field_p[x][y] >= 0) continue;
                 //put flag
                 if (field_p[x][y] == -1)  field_p[x][y] == -2;
                 else field_p[x][y] = -1;
@@ -122,5 +175,5 @@ int main()
             }
         }
     }
-   
+    system("pause>0");
 }
